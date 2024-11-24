df277beb0b9ef48e825cccf033d08a3303f8de67
Youssouf Hamidou <yousefhamid312@gmail.com>
2024-11-24 02:23:57 +0100

    V1

---
+++ discovery-sevice/src/test/java/org/example/discoverysevice/DiscoverySeviceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.discoverysevice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoverySeviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
